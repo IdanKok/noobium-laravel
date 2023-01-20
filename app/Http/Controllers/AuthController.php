fdf40ff6a2399f5a8b7bc5f273d56b8d525bb183
IdanKok <wildan.aruman90@gmail.com>
2023-01-20 20:19:10 +0700

    buat fitur sign out

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -103,4 +103,17 @@ class AuthController extends Controller
             ],
         ]);
     }
+
+    public function signOut()
+    {
+        auth()->logout();
+        return response()->json([
+            'meta'=> [
+                'code' => 200,
+                'status' => 'succes',
+                'message' =>'Signed Out Succesfully'
+            ],
+            'data'=> [],
+        ]);
+    }
 }
