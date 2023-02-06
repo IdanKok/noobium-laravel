583ac72d15890fb0e9779360be26da5a79991f29
IdanKok <wildan.aruman90@gmail.com>
2023-02-06 15:44:14 +0700

    buat fetching categories

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -21,4 +21,42 @@ class CategoryController extends Controller
             'data' => $categories
         ]);
     }
+
+    public function show($categorySlug)
+    {
+        // get category slug dimana slugnya sama dengan category slug yang didapat dari route
+        // cek apakaha category tersebut ada
+        // jika ada maka cari category berdasarkan id dari slug category tadi lalu cari articlenya
+        // lalu return article tersebut
+        // (jika kode ini dieksekusi maka category yang dicari tidak ada) maka kembalikan response error 404 category not found
+        $category = Category::where('slug', $categorySlug)->first();
+
+        if($category)
+        {
+            $articles = Category::find($category->id)
+                ->articles()
+                ->with(['category','user:id,name,picture'])
+                ->select([
+                    'id', 'user', 'category_id', 'title', 'content_preview', 'slug', 'featured_image', 'created_at', 'updated_at'
+                ])
+                ->paginate();
+                return response()->json([
+                    'meta' => [
+                        'code' => 200,
+                        'status' => 'succes',
+                        'message' => 'Article fetched succesfully',
+                    ],
+                    'data' => $articles, 
+                ]);
+        }   
+        return response()->json([
+            'meta' => [
+                'code' => 401,
+                'status' => 'error',
+                'message' => 'Category not found',
+            ],
+            'data' => [], 
+        ]);
+            
+    }
 }
